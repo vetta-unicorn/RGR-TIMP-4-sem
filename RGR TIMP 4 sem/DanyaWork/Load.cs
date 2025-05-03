1fce169930c67fd8c9e2f089256a88741ebe9ca3
Ko4erizhkas <fit12347@gmail.com>
2025-05-03 12:59:47 +0700

    Вроде готово, но я хз где провести тестирование

---
+++ RGR TIMP 4 sem/DanyaWork/Load.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using RGR_TIMP_4_sem.Models;
+
+namespace RGR_TIMP_4_sem.DanyaWork
+{
+    public class Load
+    {
+        //Deserialize From Directory
+        public List<strInTable> LoadData(string directoryPath, string searchPattern)
+        {
+            List<strInTable> items = new List<strInTable>();
+            if (Directory.Exists(directoryPath))
+            {
+                foreach (var filePath in Directory.GetFiles(directoryPath, searchPattern))
+                {
+                    try
+                    {
+                        string fileContent = File.ReadAllText(filePath);
+                        strInTable deserializeItem = JsonSerializer.Deserialize<strInTable>(fileContent);
+                        if (deserializeItem != null)
+                        {
+                            items.Add(deserializeItem);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                       throw new Exception($"Ошибка при обработки файла {filePath}:{ex.Message}");
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Данная директория не найдена!");
+            }
+            return items;
+        }
+    }
+}
