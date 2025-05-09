a17c9a57ab4f0d60a0dd46849f5d0b0fc6218516
Didi5GD <DidiFon5GD@gmail.com>
2025-05-09 21:17:16 +0700

    поменяли название Table на OneLone

---
+++ RGR TIMP 4 sem/Interfaces/IComandLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGR_TIMP_4_sem.Interfaces
+{
+    public interface IComandLine
+    {
+
+        public bool IsSelected { get; set; }
+        public int Number { get; set; }
+        public ICommand Command { get; set; }
+        public string? Str { get; set; }
+        public string? Comments { get; set; }
+
+    }
+}
--- RGR TIMP 4 sem/Models/BinAlgoritm.cs
+++ RGR TIMP 4 sem/Models/BinAlgoritm.cs
@@ -1,7 +1,10 @@
-﻿using RGR_TIMP_4_sem.Interfaces;
+﻿using DynamicData.Binding;
+using RGR_TIMP_4_sem.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,18 +17,119 @@ namespace RGR_TIMP_4_sem.Models
         /// 
         /// </summary>
         /// <param name="indexMove"> сколько действий делать за 1 нажатие, если -1 то делать весь алгоритм</param>
-        /// <param name="WorkingCell"> лист клеток</param>
+        /// <param name="WorkingCell"> лист видимых клеток </param>
         /// <param name="Table"> лист строк алгоритма</param>
-        /// <param name="position"> текущая позиция в таблице</param>
         /// <returns>возвращает 1 если выполнено, 
         /// 0 если кончились строки, 
         /// -1 если бесконечный цикл</returns>
-        public int Working (int indexMove, List <ICell> WorkingCell, List <ITable> Table, int position)
+        public string Working (int indexMove, ObservableCollection<ICell> Cells, ObservableCollection<IComandLine> ComandLine)
         {
+            if(Cells==null)
+            {
+                throw new NullReferenceException("The cell list is empty");
+            }
+            else if (ComandLine == null)
+            {
+                throw new NullReferenceException("The Comands list is empty");
+            }
+            else {
+                
+                int flag = 1;
+                int count_cycle = 0;
+                while (flag!=-1)
+                {
+                    int now = Number_of_SelectedStr(ComandLine);
+                    if (now==indexMove)
+                    {
+                        return "The commands were successfully completed.";
+                    }
+                    if(count_cycle==1000)
+                     {
+                         return "The endless loop";
+                     }
+                    try
+                    {
+                        if (ComandLine[now].Command is Question)
+                        {
+                            flag =ComandLine[now].Command.Work(Cells);
+                            int[] mass = Split(ComandLine[now].Str);
+                            switchNumberLine(ComandLine, mass[flag]);
+                            count_cycle++;
+                         }
+                        else
+                        {
+                            flag = ComandLine[now].Command.Work(Cells);
+                            switchNumberLine(ComandLine, now+1);
+                        }
+                    
+                    }
+                    catch (NullReferenceException)
+                    {
+                          
+                    }
+
+
+                } 
+            }
 
 
 
-            return 0;
+
+            return " ";
+        }
+
+        public int[] Split(string str)
+        {
+            string[] parts = str.Split(',');
+            int[] mass = new int [2];
+            for(int i =0; i<2; i++)
+            {
+                mass[i] = Convert.ToInt32(parts[i]);
+            }
+            return mass;
+        }
+
+
+        
+        public int Number_of_SelectedStr(ObservableCollection<IComandLine> ComandLine)
+        {
+            if (ComandLine == null)
+            {
+                throw new NullReferenceException("The Comands list is empty");
+            }
+            else
+            {
+                int index = 0;
+                foreach (var t in ComandLine)
+                {
+                    if (t.IsSelected)
+                    {
+                        break;
+                    }
+                    else { index++; }
+                }
+                return index;
+            }
+        }
+
+        public void switchNumberLine(ObservableCollection<IComandLine> ComandLine, int index)
+        {
+            if (ComandLine == null)
+            {
+                throw new NullReferenceException("The Comands list is empty");
+            }
+            else
+            {
+                foreach (var t in ComandLine)
+                {
+                    if (t.Number == index)
+                    {
+                        t.IsSelected = true;
+                        break;
+                    }
+                    else { t.IsSelected = false; }
+                }
+            }
         }
     }
 }
